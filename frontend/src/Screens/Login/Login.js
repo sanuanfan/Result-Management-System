1795188eddfdc94f84c2d0d56e7f3dfa16ce563a
Fidahasm <fidahkt@gmail.com>
2024-08-24 00:38:43 +0530

    Files created

---
+++ frontend/src/Screens/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Login/Login.css'
+
+function Login() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
---
+++ frontend/src/Screens/Student/HomStudent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomStudent() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomStudent
