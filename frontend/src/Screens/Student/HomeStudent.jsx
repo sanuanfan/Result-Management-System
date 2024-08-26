f4f0e518600b0406bea4a40d27a92967d1209df4
sanuanfan <sanuanfan8535@gmail.com>
2024-08-27 01:48:41 +0530

    student page started

--- frontend/src/Screens/Student/HomeStudent.jsx
+++ frontend/src/Screens/Student/HomeStudent.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import '../Student/student.css'
 
 function HomeStudent() {
   return (
-    <div>
+    <div className='main-container'>
+      <div className="nav-bar">
+
+      </div>
+      <div className="sub-container">
+        <div className="mini-container">
+          <h3>Mark Display</h3>
+          <div className='search'>
+                <input type="password" placeholder='Password' />
+                <i className='bx bxs-key' id='search-icon'></i>
+              </div>
+        </div>
+      </div>
+      
       
     </div>
   )
