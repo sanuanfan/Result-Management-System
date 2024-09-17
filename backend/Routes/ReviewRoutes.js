2c90ff45185b43d46135124d157b0eb34ce90522
sanuanfan <sanuanfan8535@gmail.com>
2024-09-17 21:46:21 +0530

    multer updated

--- backend/Routes/ReviewRoutes.js
+++ backend/Routes/ReviewRoutes.js
@@ -7,7 +7,29 @@ const xlsx = require('xlsx');
 const reviewModel = require('../model/ReviewModel'); // Assuming the model is named ReviewModel
 const router = express.Router();
 
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
+
+
+const storage = multer.diskStorage({
+  destination: async (req, file, cb) => {
+    try {
+      const destDir = "./uploads/";
+      if (!fs.existsSync(destDir)) {
+        fs.mkdirSync(destDir, { recursive: true });
+      }
+      cb(null, destDir);
+    } catch (error) {
+      console.error("Error in destination:", error);
+      cb(error, "./uploads/");
+    }
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix + ext);
+  },
+});
+const upload = multer({ storage: storage });
 
 // Utility function to convert Excel to CSV
 const excelToCsv = (filePath) => {
--- backend/Routes/StudentRoute.js
+++ backend/Routes/StudentRoute.js
@@ -6,7 +6,29 @@ const path = require('path');
 const xlsx = require('xlsx');
 const StudentModel = require('../model/StudentModel');
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
+
+
+const storage = multer.diskStorage({
+  destination: async (req, file, cb) => {
+    try {
+      const destDir = "./uploads/";
+      if (!fs.existsSync(destDir)) {
+        fs.mkdirSync(destDir, { recursive: true });
+      }
+      cb(null, destDir);
+    } catch (error) {
+      console.error("Error in destination:", error);
+      cb(error, "./uploads/");
+    }
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix + ext);
+  },
+});
+const upload = multer({ storage: storage });
 
 
 const excelToCsv = (filePath) => {
